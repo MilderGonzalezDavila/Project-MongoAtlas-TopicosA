494164bd5131a1caaec1e12483250772a454c81a
unknown <mgonzalezd23_1@unc.edu.pe>
2025-11-07 11:41:05 -0500

    Isertando el primero elementos a la base de datos biblioteca25

--- notebook/Connect.py
+++ notebook/Connect.py
@@ -5,7 +5,7 @@ import os
 # Cargar variables de entorno
 load_dotenv()
 
-MONGODB_URI = os.getenv('MONGODV_URI_ATLAS')
+MONGODB_URI = os.getenv('MONGODB_URI_ATLAS')
 DB_NAME_ATLAS = os.getenv('MONGODB_DATA')
 
 try:
@@ -18,4 +18,3 @@ except errors.OperationFailure as e:
     print('Erro de auntenticacion', e)
 except Exception as e:
     print('No se que pasó')
-
